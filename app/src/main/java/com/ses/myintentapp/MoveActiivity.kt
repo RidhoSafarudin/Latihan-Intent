9b8c5efe753dda01f092c6d9ba0a64dd26b6fb26
RidhoSafarudin <ridhosafarudin10@gmail.com>
2020-10-01 16:30:18 +0700

    first commit

---
+++ app/src/main/java/com/ses/myintentapp/MoveActiivity.kt
@@ -0,0 +1,11 @@
+package com.ses.myintentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActiivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_actiivity)
+    }
+}
